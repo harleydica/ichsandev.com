47f840b2e40dccf1edd41910e94e00f7af7567c1
Muhammad Ichsanul Fadhil <69036480+ichsanputr@users.noreply.github.com>
2023-01-03 19:36:57 +0700

    rebuild: changes #2

--- src/components/MDXComponents/MDXComponents.tsx
+++ src/components/MDXComponents/MDXComponents.tsx
@@ -6,19 +6,15 @@ import ItemGrid from './ItemGrid'
 import Kbd from './Kbd'
 import Pre from './Pre'
 import Tree from './Tree'
-import Colors from '../Design/Colors'
 
 const MDXComponents = {
   a: Link,
   Image,
   pre: Pre,
-
-  // Custom components
   ItemGrid,
   Callout,
   Kbd,
   Tree,
-  Colors,
 }
 
 export default MDXComponents
